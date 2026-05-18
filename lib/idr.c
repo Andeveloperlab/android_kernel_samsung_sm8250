ad620a77ac490c5e01d602f2d7e6358d317b959e
Andeveloperlab <yogurtbirsah65@gmail.com>
2026-05-19 00:38:05 +0300

    Revert "lib/idr.c: remove simple_ida_lock"
    This reverts commit 02947c371276e87e321211683a3dcde12e31834d.

--- lib/idr.c
+++ lib/idr.c
@@ -544,18 +544,11 @@ int ida_alloc_range(struct ida *ida, unsigned int min, unsigned int max,
 		max = INT_MAX;
 
 again:
-	if (!ida_pre_get(ida, gfp_mask))
-		return -ENOMEM;
-
 	xa_lock_irqsave(&ida->ida_rt, flags);
-	ret = ida_get_new_above(ida, start, &id);
-	if (!ret) {
-		if (id > max) {
-			ida_remove(ida, id);
-			ret = -ENOSPC;
-		} else {
-			ret = id;
-		}
+	id = ida_get_new_above(ida, min);
+	if (id > (int)max) {
+		ida_remove(ida, id);
+		id = -ENOSPC;
 	}
 	xa_unlock_irqrestore(&ida->ida_rt, flags);
 
