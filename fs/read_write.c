8ceadbe9886c016db76b62bf5f3cadeec431377e
Andeveloperlab <yogurtbirsah65@gmail.com>
2026-05-19 19:11:29 +0300

    fixup!

--- fs/read_write.c
+++ fs/read_write.c
@@ -572,6 +572,19 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 }
 
 ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
+{
+	struct fd f = fdget_pos(fd);
+	ssize_t ret = -EBADF;
+
+	if (f.file) {
+		loff_t pos = file_pos_read(f.file);
+		ret = vfs_read(f.file, buf, count, &pos);
+		if (ret >= 0)
+			file_pos_write(f.file, pos);
+		fdput_pos(f);
+	}
+	return ret;
+}
 
 #if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
 extern bool ksu_vfs_read_hook __read_mostly;
@@ -596,11 +609,6 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 		fdput_pos(f);
 	}
 	return ret;
-}
-
-SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
-{
-	return ksys_read(fd, buf, count);
 }
 
 ssize_t ksys_write(unsigned int fd, const char __user *buf, size_t count)
